660b3e1f277f4ac4a1a7ef73b27b666efa302baa
Noashi <50229214+Noashi@users.noreply.github.com>
2019-05-18 17:18:15 +0900

    Update const.php
    個人情報を消去

--- Include/const/const.php
+++ Include/const/const.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_HOST',   'localhost'); // データベースのホスト名又はIPアドレス
-define('DB_USER',   'naopidesu');  // MySQLのユーザ名
-define('DB_PASSWD', 'la1misma');    // MySQLのパスワード
-define('DB_NAME',   'naopidesu');    // データベース名
+define('DB_USER',   '');  // MySQLのユーザ名
+define('DB_PASSWD', '');    // MySQLのパスワード
+define('DB_NAME',   '');    // データベース名
 
 define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
 define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
